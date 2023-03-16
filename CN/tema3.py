69b948156a0688a6e1c91a6fca99431de041bfab
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2023-03-16 23:25:44 +0200

    add resolveLiniarSystem

--- CN/tema3.py
+++ CN/tema3.py
@@ -10,7 +10,10 @@ try:
     import matplotlib.pyplot as plt
 except:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib"])
-
+try:
+    import scipy
+except:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "scipy"])
 def log(type, message,error = False):
     if error == True:
         print("[" + type + "] "+" [ERRO] " + message)
@@ -31,25 +34,25 @@ def HouseholderAlg(A,b):
     Qbarat = createInMatrix(A.shape[0])
     # A = QR
     for r in range(n-1):
-        print("r=",r)
+        # print("r=",r)
         # u=np.zeros(A.shape[0])
         # cosntruct Pr - projection matrix 
         # sigma = sign(a[r+1][r]) * ||a[r+1][r]||_2
         sigma = 0
         for i in range(r, n):
-            print(f"A[{i}][{r}]={A[i][r]}")
+            # print(f"A[{i}][{r}]={A[i][r]}")
             sigma += A[i][r]**2
         if sigma < epsilon:
             # r=r+1 if and only if Pr = In
             print('break')
             break
-        print("sigma=",sigma)
+        # print("sigma=",sigma)
         k = sigma**0.5
-        print("k=",k)
+        # print("k=",k)
         if A[r][r] >0:
             k = -k
         beta = sigma - k*A[r][r]
-        print("beta=",beta)
+        # print("beta=",beta)
         # construct Pr
         u[r] = A[r][r] - k
         for i in range(r+1, n):
@@ -85,7 +88,7 @@ def HouseholderAlg(A,b):
 
         # Qbarat = Qbarat*Pr
         # transformarea coloanelor j = r+1,...,n-1
-        print("Qbarat before", Qbarat)
+        # print("Qbarat before", Qbarat)
         for j in range(n):
             y=0
             for i in range(r, n):
@@ -94,21 +97,21 @@ def HouseholderAlg(A,b):
             for i in range(r, n):
                 Qbarat[i][j] -= u[i]*y
             
-        print("R", transpusa(Qbarat))
-        print("Q", A)
+        # print("R", transpusa(Qbarat))
+        # print("Q", A)
     return transpusa(Qbarat),A
 
 # test
 A = np.array([[0,0,4],[1,2,3],[0,1,2]])
-s = np.array([3,2,1])
+s = np.array([4,10,4])
 # print(transpusa(A))
 # q,r = HouseholderAlg(A,s)
-q,r=HouseholderAlg(A,s)
+# q,r=HouseholderAlg(A,s)
 
-print("Q", q,'\n')
-print("R", r,'\n')
-# multiplication q r
-print("q r", np.matmul(q,r))
+# print("Q", q,'\n')
+# print("R", r,'\n')
+# # multiplication q r
+# print("q r", np.matmul(q,r))
 
 
 
@@ -121,7 +124,80 @@ def get_b_vector(n,A,s):
         for j in range(n):
             sum += A[i][j]*s[j]
         b[i] = sum
-    return b
+    reversedX = np.array([0]*n)
+    for i in range(n):
+        reversedX[i] = b[n-i-1]
+    return reversedX
 
 # print(get_b_vector(3,A,s))
+
+def doMultimplication(A,b):
+    n = len(b)
+    x = np.array([0]*n)
+    for i in range(n):
+        sum = 0
+        for j in range(n):
+            sum += A[i][j]*b[j]
+        x[i] = sum
         
+    #  reverse x
+    reversedX = np.array([0]*n)
+    for i in range(n):
+        reversedX[i] = x[n-i-1]
+    return reversedX
+
+def printSystem(A,b):
+    # print x1 + x2 + x3 = 3
+    #       x1 + 2x2 + 3x3 = 2
+    for i in range(A.shape[0]):
+        if(i == 0):
+            print("Sistemul este: \n\t",end='')
+        for j in range(A.shape[1]):
+            if A[i,j] !=0:
+                if j == A.shape[1]-1:
+                    if A[i,j] == 1:
+                        print(f"x{j+1}", end='')  
+                    else:
+                            print(f"{A[i,j]}*x{j+1}", end='')
+                else:
+                    if A[i,j] == 1:
+                        print(f"x{j+1}+", end='')  
+                    else:
+                        print(f"{A[i,j]}*x{j+1}+", end='')
+        if i == A.shape[0]-1:
+            print(f" = {b[i]}")
+        else:                
+            print(f" = {b[i]}\n\t",end='')
+
+
+def resolveLiniarSystem(A,b,sciHub = False):
+    n= A.shape[0]
+    x = np.array([0]*n)
+    # printSystem(A,b)
+    if sciHub:
+        q, r = scipy.linalg.qr(A)
+    else:
+        q,r = HouseholderAlg(A,b)
+        #  rezolvam sistemul R*x = Q^T*b
+    q = transpusa(q)
+    # for my algorithm
+    QtB = doMultimplication(q,b)
+    # for sciHub
+    if sciHub:
+        QtB = np.matmul(q,b)
+    # print(r,QtB)
+    # rezolvam sistemul R*x = QtB
+    printSystem(r,QtB)
+ 
+    for i in range(n,0,-1):
+        sum = 0
+        for j in range(i,n):
+            sum += r[i-1][j]*x[j]
+        x[i-1] = (QtB[i-1] - sum)/r[i-1][i-1]
+
+    print("Solutia este: ",x)
+    
+
+    
+resolveLiniarSystem(A,s,True)
+resolveLiniarSystem(A,s,False)
